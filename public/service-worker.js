f9c0492bcb0de463ca6d0965e218675045b56067
Ryan Allen <ryan.c.allen.work@gmail.com>
2021-11-19 19:30:47 -0800

    fixed routes and added download icon

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,27 +1,17 @@
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
-    "/assets/css/style.css",
-    "/assets/js/loadPosts.js",
-    "/assets/images/Angular-icon.png",
-    "/assets/images/React-icon.png",
-    "/assets/images/Vue.js-icon.png",
-    "/manifest.webmanifest",
-    "/favicon.ico",
-    "/assets/images/icons/icon-72x72.png",
-    "/assets/images/icons/icon-96x96.png",
-    "/assets/images/icons/icon-128x128.png",
-    "/assets/images/icons/icon-144x144.png",
-    "/assets/images/icons/icon-152x152.png",
-    "/assets/images/icons/icon-192x192.png",
-    "/assets/images/icons/icon-384x384.png",
-    "/assets/images/icons/icon-512x512.png",
+    "/styles.css",
+    "/index.js",
+    "/db.js",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
   ];
   
   const CACHE_NAME = "static-cache-v2";
   const DATA_CACHE_NAME = "data-cache-v1";
   
-  // install
+  // INSTALL
   self.addEventListener("install", function(evt) {
     evt.waitUntil(
       caches.open(CACHE_NAME).then(cache => {
@@ -50,7 +40,7 @@ const FILES_TO_CACHE = [
     self.clients.claim();
   });
   
-  // fetch
+  // FETCH
   self.addEventListener("fetch", function(evt) {
     // cache successful requests to the API
     if (evt.request.url.includes("/api/")) {
@@ -75,8 +65,6 @@ const FILES_TO_CACHE = [
       return;
     }
   
-    // if the request is not for the API, serve static assets using "offline-first" approach.
-    // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
     evt.respondWith(
       caches.match(evt.request).then(function(response) {
         return response || fetch(evt.request);
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -17,7 +17,9 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useCreateIndex:true,
+  useUnifiedTopology:true
 });
 
 // routes
@@ -26,3 +28,4 @@ app.use(require("./routes/api.js"));
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
